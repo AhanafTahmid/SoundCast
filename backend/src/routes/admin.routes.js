635be65247bf1a182e0233173a10fe9026f13e54
Shapin199 <mdtanvir054321@gmail.com>
2025-04-07 18:21:27 +0600

    testing

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,7 +1,10 @@
-import {Router} from "express"
-import {getAdmin} from "../controller/admin.controller.js"
+import { Router } from "express";
+import { createSong } from "../controller/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+
 
 const router = Router();
-router.get('/', getAdmin)
 
-export default router
+router.get("/", protectRoute, requireAdmin, createSong)
+
+export default router;
