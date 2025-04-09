97ea3f38ecece11952dd9eb61a6f6ed8532895fd
Shapin199 <mdtanvir054321@gmail.com>
2025-04-10 02:54:08 +0600

    song route and controller

--- backend/src/routers/song.route.js
+++ backend/src/routers/song.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTradingSongs } from "../controller/song.controller.js";
+import { protectRoute,requireAdmin} from "../middleware/auth.middleware.js"
 
 
 const router = Router();
 
-router.get("/",(req,res) => {
-    res.send("song route with Get method")
-})
+router.get("/",protectRoute,requireAdmin,getAllSongs);
+router.get("/featured",getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trading", getTradingSongs)
 
 export default router;
