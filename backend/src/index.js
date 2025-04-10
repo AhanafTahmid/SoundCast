16dcea9a64dc42e2f4b9b3f2472e21df5cb5789d
Shapin199 <mdtanvir054321@gmail.com>
2025-04-10 14:42:45 +0600

    done the route work in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import authRouters from './routers/auth.route.js'
 import adminRouters from './routers/admin.route.js'
 import songRouters from './routers/song.route.js'
 import albumRouters from './routers/album.route.js'
-import startRouters from './routers/star.route.js'
+import startRouters from './routers/stat.route.js'
 import { connectDB } from './lib/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import fileupload from 'express-fileupload'
@@ -32,7 +32,7 @@ app.use("/api/auth",authRouters);
 app.use("/api/admin",adminRouters);
 app.use("/api/songs",songRouters);
 app.use("/api/albums",albumRouters);
-app.use("/api/stars",startRouters)
+app.use("/api/stats",startRouters)
 
 // error handling
 app.use((err, req, res, next) => {
@@ -43,3 +43,5 @@ app.listen(PORT,() => {
     console.log('Server is running on port',PORT);
     connectDB();
 })
+
+// todo socket io 
--- backend/src/routers/star.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/",(req,res) => {
-    res.send("star route with Get method")
-})
-
-export default router;
