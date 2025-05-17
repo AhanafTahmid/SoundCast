d1309c658620c45c06b67632d134e315a9631e06
AhanafTahmid <ahanaftahmid55@gmail.com>
2025-05-18 00:51:44 +0600

    Built Chat Page UI + Custom 404 Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,31 @@
-import { Routes,Route } from "react-router-dom";
-import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
+import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
 import AlbumPage from "./pages/album/AlbumPage";
 import AdminPage from "./pages/Admin/AdminPage";
+
 import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route
-				   path="sso-callback"
-				   element={<AuthenticateWithRedirectCallback signInFallbackRedirectUrl={"/auth-callback"}/>} 
+					path='/sso-callback'
+					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
 				/>
-				<Route path="/auth-callback" element={<AuthCallBackPage />} />
-				<Route path="/admin" element={<AdminPage />} />
+				<Route path='/auth-callback' element={<AuthCallBackPage />} />
+				<Route path='/admin' element={<AdminPage />} />
+
 				<Route element={<MainLayout />}>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/chat" element={<ChatPage />} />
-				<Route path="/albums/:albumId" element={<AlbumPage />} />
+					<Route path='/' element={<HomePage />} />
+					<Route path='/chat' element={<ChatPage />} />
+					<Route path='/albums/:albumId' element={<AlbumPage />} />
+					<Route path='*' element={<NotFoundPage />} />
 				</Route>
 			</Routes>
 			<Toaster />
--- frontend/src/stores/usePlayerStore.tsx
+++ frontend/src/stores/usePlayerStore.tsx
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { Song } from "@/types";
+import { useChatStore } from "./useChatStore";
 
 interface PlayerStore {
 	currentSong: Song | null;
@@ -33,6 +34,14 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 		if (songs.length === 0) return;
 
 		const song = songs[startIndex];
+
+		const socket = useChatStore.getState().socket;
+		if (socket.auth) {
+			socket.emit("update_activity", {
+				userId: socket.auth.userId,
+				activity: `Playing ${song.title} by ${song.artist}`,
+			});
+		}
 		set({
 			queue: songs,
 			currentSong: song,
@@ -43,7 +52,16 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 
 	setCurrentSong: (song: Song | null) => {
 		if (!song) return;
-        const songIndex = get().queue.findIndex((s) => s._id === song._id);
+
+		const socket = useChatStore.getState().socket;
+		if (socket.auth) {
+			socket.emit("update_activity", {
+				userId: socket.auth.userId,
+				activity: `Playing ${song.title} by ${song.artist}`,
+			});
+		}
+
+		const songIndex = get().queue.findIndex((s) => s._id === song._id);
 		set({
 			currentSong: song,
 			isPlaying: true,
@@ -54,6 +72,16 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 	togglePlay: () => {
 		const willStartPlaying = !get().isPlaying;
 
+		const currentSong = get().currentSong;
+		const socket = useChatStore.getState().socket;
+		if (socket.auth) {
+			socket.emit("update_activity", {
+				userId: socket.auth.userId,
+				activity:
+					willStartPlaying && currentSong ? `Playing ${currentSong.title} by ${currentSong.artist}` : "Idle",
+			});
+		}
+
 		set({
 			isPlaying: willStartPlaying,
 		});
@@ -63,9 +91,18 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 		const { currentIndex, queue } = get();
 		const nextIndex = currentIndex + 1;
 
-		// play next song, if there is one
+		// if there is a next song to play, let's play it
 		if (nextIndex < queue.length) {
 			const nextSong = queue[nextIndex];
+
+			const socket = useChatStore.getState().socket;
+			if (socket.auth) {
+				socket.emit("update_activity", {
+					userId: socket.auth.userId,
+					activity: `Playing ${nextSong.title} by ${nextSong.artist}`,
+				});
+			}
+
 			set({
 				currentSong: nextSong,
 				currentIndex: nextIndex,
@@ -74,9 +111,16 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 		} else {
 			// no next song
 			set({ isPlaying: false });
+
+			const socket = useChatStore.getState().socket;
+			if (socket.auth) {
+				socket.emit("update_activity", {
+					userId: socket.auth.userId,
+					activity: `Idle`,
+				});
+			}
 		}
 	},
-
 	playPrevious: () => {
 		const { currentIndex, queue } = get();
 		const prevIndex = currentIndex - 1;
@@ -84,14 +128,31 @@ export const usePlayerStore = create<PlayerStore>((set, get) => ({
 		// theres a prev song
 		if (prevIndex >= 0) {
 			const prevSong = queue[prevIndex];
+
+			const socket = useChatStore.getState().socket;
+			if (socket.auth) {
+				socket.emit("update_activity", {
+					userId: socket.auth.userId,
+					activity: `Playing ${prevSong.title} by ${prevSong.artist}`,
+				});
+			}
+
 			set({
 				currentSong: prevSong,
 				currentIndex: prevIndex,
 				isPlaying: true,
 			});
 		} else {
-			// no previous song
+			// no prev song
 			set({ isPlaying: false });
+
+			const socket = useChatStore.getState().socket;
+			if (socket.auth) {
+				socket.emit("update_activity", {
+					userId: socket.auth.userId,
+					activity: `Idle`,
+				});
+			}
 		}
 	},
 }));
