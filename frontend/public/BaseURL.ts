59be510921c8f677e3b34fc89b23dbfeac8b547b
Ahanaf Tahmid <45955172+AhanafTahmid@users.noreply.github.com>
2025-08-09 01:53:44 +0600

    Update BaseURL.ts

--- frontend/public/BaseURL.ts
+++ frontend/public/BaseURL.ts
@@ -1,2 +1,4 @@
 //export const API_BASE_URL = import.meta.env.BASEE_URL;
-export const API_BASE_URL = "http://localhost:5000";
+//export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://soundcast.onrender.com";
+
