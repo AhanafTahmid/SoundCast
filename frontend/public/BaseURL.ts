1e240c542321ac9143fb498572fd2b48e1c0676c
AhanafTahmid <ahanaftahmid55@gmail.com>
2025-08-09 01:59:18 +0600

    updated audit and errors fixed

--- frontend/public/BaseURL.ts
+++ frontend/public/BaseURL.ts
@@ -1,4 +1,3 @@
-//export const API_BASE_URL = import.meta.env.BASEE_URL;
-//export const API_BASE_URL = "http://localhost:5000";
-export const API_BASE_URL = "https://soundcast.onrender.com";
+export const API_BASE_URL = import.meta.env.BASEE_URL;
+
 
--- frontend/src/pages/VoiceRecorder/VoiceRecorderPage.tsx
+++ frontend/src/pages/VoiceRecorder/VoiceRecorderPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import VoiceRecorder from "../../components/VoiceRecorder";
 
 const VoiceRecorderPage = () => {
